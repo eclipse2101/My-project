0eba077880cde10092635888dff4292d3351124b
explosiveseccc <rdericieux25@aoiths.org>
2024-06-12 12:47:51 -0400

    even more testing

--- Assets/scripts/movementScript.cs
+++ Assets/scripts/movementScript.cs
@@ -7,7 +7,7 @@ public class movementScript : MonoBehaviour
     public float vertical;
     public float horizontial; 
     public float speed;
-    public float jumpPower; 
+    public float jumpPower;  
     public bool isGrounded = true;
     Rigidbody player; 
     // Start is called before the first frame update
@@ -24,10 +24,21 @@ public class movementScript : MonoBehaviour
 
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontial);
 
-        if( vertical > 0 && isGrounded == true)
+        if((vertical > 0.4f && isGrounded == true) || (Input.GetKeyDown(KeyCode.Space) && isGrounded == true))
         {
            isGrounded = false;
            player.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+           vertical = 0;
+        }
+
+        if (vertical < -0.1f && isGrounded == true)
+        {
+            Debug.Log("The player is crouching");
+        }
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Button was Pressed");
         }
     }
 
